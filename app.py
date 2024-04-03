0f34d7c83d6bb0e07d9c43f29e6a0beda494c0d1
TrixodeStudios <165813559+TrixodeStudios@users.noreply.github.com>
2024-04-03 10:29:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,40 +9,19 @@ import ffmpeg
 from scipy.spatial import distance
 
 
-def create_app():
-    app = Flask(__name__, static_folder='uploads', static_url_path='/uploads')
-    app.config['UPLOAD_FOLDER'] = '/app/uploads/'
-    upload_folder = app.config['UPLOAD_FOLDER']
-    if not os.path.exists(upload_folder):
-        os.makedirs(upload_folder)
-    # Other setup code...
-    return app
+app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-app = create_app()
-
-@app.route('/', methods=['GET'])
-def homepage():
-    return "Homepage"
-
-
-@app.route('/hello', methods=['GET'])
-def hello():
-    return "Hello"
-
-@app.route('/get_similar', methods=['POST'])
-def cosine_similarity():
+@app.route('/postdata', methods=['POST'])
+def postdata():
+    # Get JSON data from the request
     data = request.json
-    query_vector = data['query_vector']
-    vector_text_pairs = data['vectors']
+    print("Received data:", data)
+    # Respond with a success message
+    return jsonify({"success": True, "msg": "Data received"}), 200
 
-    # Extract embeddings and their corresponding texts
-    vectors = [pair['embeddings'] for pair in vector_text_pairs]
-    texts = [pair['text'] for pair in vector_text_pairs]
-
-    # Calculate cosine similarity for each vector
-    # Return the index of the most similar vector
-    most_similar_index = max(range(len(vectors)), key=lambda index: 1 - distance.cosine(query_vector, vectors[index]))
-
-    return jsonify({'most_similar_text': texts[most_similar_index]})
-
+if __name__ == '__main__':
+    app.run(debug=True)
