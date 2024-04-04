3aad0e4d7f93c1013fb152ee0f66b46f5dc05a17
TrixodeStudios <165813559+TrixodeStudios@users.noreply.github.com>
2024-04-04 11:32:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import logging
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
 
 @app.route('/', methods=['GET'])
 def homepage():
@@ -11,10 +13,14 @@ def homepage():
 def hello():
     return "Hello"
 
+
 @app.route('/postdata', methods=['POST'])
 def postdata():
     data = request.json
     print("Received data:", data)
+    app.logger.info(f'Received data: {data}')  # Log the received data
     return jsonify({"success by Trixode-Studios": True, "msg": "Data received"}), 200
+
+
 
 
